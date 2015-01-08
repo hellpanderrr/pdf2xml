d289b029c9de0df82d58bab5ba84b38243afe86f
hellpanderrr <hellpanderrr@users.noreply.github.com>
2015-01-08 07:30:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,16 @@ import sys
 from collections import Counter
 import matplotlib.pyplot as plt
 import networkx as nx
+from lxml import etree as ET
+from HTMLParser import HTMLParser
+
+def find_keys(d,key,area,r=True,l=True):
+        key = int(key)
+        keys = []
+        for j in range(key-area*l,key+area*r):
+            if d.has_key(j):
+                keys.append(j)
+        return keys
 def get_chars(line,with_anno=False):
     #get chars from the LTTextline
     ret = []
@@ -41,26 +51,79 @@ def get_objects(layout):
         else:
             objs.append(obj)
     return objs
-def _build_annotations( self, page ):
-        for annot in page.annots.resolve():
-            if isinstance( annot, PDFObjRef ):
-                annot= annot.resolve()
-                assert annot['Type'].name == "Annot", repr(annot)
-                if annot['Subtype'].name == "Widget":
-                    if annot['FT'].name == "Btn":
-                        assert annot['T'] not in self.fields
-                        self.fields[ annot['T'] ] = annot['V'].name
-                    elif annot['FT'].name == "Tx":
-                        assert annot['T'] not in self.fields
-                        self.fields[ annot['T'] ] = annot['V']
-                    elif annot['FT'].name == "Ch":
-                        assert annot['T'] not in self.fields
-                        self.fields[ annot['T'] ] = annot['V']
-                        # Alternative choices in annot['Opt'] )
-                    else:
-                        raise Exception( "Unknown Widget" )
-            else:
-                raise Exception( "Unknown Annotation" )
+def find_tables(objs):
+    regobjs = tableobjs = None
+    if objs:
+        group = []
+        for i in objs:
+            n=0
+            #if type(i) == pdfminer.layout.LTTextLineHorizontal:
+            tmp = []
+            for j in objs:
+                    if j.hdistance(i) < 10 and j.vdistance(i) < 10 and j <> i and (type(j) == pdfminer.layout.LTRect  or type(j) == pdfminer.layout.LTLine) :
+                        n+=1
+                        tmp.append(j)
+            if n>3:
+                group.append(i)
+                group+=tmp
+        if group:                
+            y0 = min(sorted(o, key=lambda x:x.y0, reverse=True), key = lambda x:x.y0).y0        
+            y1 = max(sorted(o, key=lambda x:x.y0, reverse=True), key = lambda x:x.y0).y0     
+            x0 = min(sorted(o, key=lambda x:x.x0, reverse=True), key = lambda x:x.x0).x0        
+            x1 = max(sorted(o, key=lambda x:x.x0, reverse=True), key = lambda x:x.x0).x0 
+            print x0,y0,x1,y1
+            for i in sorted(objs_r,key=lambda x:x.y0,reverse=True):
+
+                if  x0 <=i.x0 <= x1 and y0 <=i.y0 <= y1:
+                    if hasattr(i,'get_text'):
+                        pass#print i.get_text(),i
+            tableobjs = [i for i in objs if x0 <=i.x0 <= x1 and y0 <=i.y0 <= y1]
+            print len(objs),len(tableobjs)
+            regobjs = list(set(objs)-set(tableobjs))
+        else:
+            regobjs=objs
+    return regobjs,tableobjs
+def gen_xml(objs,n,xml):
+    as_list = []
+    for i in sorted(sort_by_x(objs)):
+        as_list.append(sort_by_x(objs)[int(i)])       
+    as_list = sum(as_list,[]) 
+    f = []
+    for i in as_list:
+        size = round(min([j.size for j in get_chars(i)]),2)
+        if i.get_text().strip():
+           # print '** %s **' %i.get_text(),round(min([j.size for j in get_chars(i)]),2)
+            f.append(round(min([j.size for j in get_chars(i)]),2))
+    f = list(set(f))
+    c = Counter(f)   
+    header = xml.find('header')
+    body = xml.find('body')   
+    for i in as_list:
+                size = round(min([j.size for j in get_chars(i)]),2)            
+                text = i.get_text()
+                if size == max(f) and n==0:
+                    header.text += text
+                if  c.most_common(1)[0][0] < size < max(f):                  
+                    body.text +='<h1>'+text.replace('\n','')+'</h1>'
+                elif f[-2] < size < f[-1]:
+                    body.text +='<h2>'+text.replace('\n','')+'</h2>'
+                else: 
+                    body.text += text.replace('\n','') if '\n' in text and text.strip() else text
+                    print repr( text.replace('\n','') if '\n' in text and text.strip() else text)
+                    
+    return xml
+def make_xml(objs,objs_r,objs_l,n,xml):
+    
+    if objs:
+        regobjs=find_tables(objs)[0]
+        xml = gen_xml(regobjs,n,xml)
+    if objs_l:
+        regobjs=find_tables(objs_l)[0]
+        xml = gen_xml(regobjs,n,xml)
+    if objs_r:
+        regobjs=find_tables(objs_r)[0]
+        xml = gen_xml(regobjs,n,xml)
+    return xml
 def get_fonts(objs):
     """Returns a set with rounded font sizes"""
     fonts = set()
@@ -97,94 +160,11 @@ def sort_by_x(objs,decimals=-1,has_text=True):
         if not i:
             s.remove(i)
     for i in s.items():
-            s[i[0]] = sorted(i[1],key=lambda x:x.y0,reverse=True)
+            s[i[0]] = sorted(i[1],key=lambda x:x.y1,reverse=True)
     return s
-def make_xml(l,q):
-    
-    for i in l:
-            size = round(get_chars(i)[0].size,2)
-            text = i.get_text()
-            if size > min(fonts):
-                q+='<strong>'+text+'</strong>'
-            elif size == min(fonts):
-                q+= text.replace('\n','')
-            elif size == max(fonts):
-                q+= '<header>'+text+'</header>'
-    return q
-def find_tables(objs):
-    regobjs = None
-    if objs:
-        o = []
-        for i in objs:
-            n=0
-            #if type(i) == pdfminer.layout.LTTextLineHorizontal:
-            tmp = []
-            for j in objs:
-                    if j.hdistance(i) < 10 and j.vdistance(i) < 10 and j <> i and (type(j) == pdfminer.layout.LTRect  or type(j) == pdfminer.layout.LTLine) :
-                        n+=1
-                        tmp.append(j)
-                        #print j.width,j.height,type(j)
-            if n>3:
-                o.append(i)
-                o+=tmp
-        if o:                
-            y0 = min(sorted(o, key=lambda x:x.y0, reverse=True), key = lambda x:x.y0).y0        
-            y1 = max(sorted(o, key=lambda x:x.y0, reverse=True), key = lambda x:x.y0).y0     
-            x0 = min(sorted(o, key=lambda x:x.x0, reverse=True), key = lambda x:x.x0).x0        
-            x1 = max(sorted(o, key=lambda x:x.x0, reverse=True), key = lambda x:x.x0).x0 
-            print x0,y0,x1,y1
-            for i in sorted(objs_r,key=lambda x:x.y0,reverse=True):
 
-                if  x0 <=i.x0 <= x1 and y0 <=i.y0 <= y1:
-                    if hasattr(i,'get_text'):
-                        pass#print i.get_text(),i
-            tableobjs = [i for i in objs if x0 <=i.x0 <= x1 and y0 <=i.y0 <= y1]
-            print len(objs),len(tableobjs)
-            regobjs = list(set(objs)-set(tableobjs))
-        else:
-            regobjs=objs
-    return regobjs
-def gen_xml(objs,n):
-    q=''
-    l=[]
-    for i in sorted(sort_by_x(objs)):
-        l.append(sort_by_x(objs)[int(i)])       
-    l = sum(l,[]) 
-    f = []
-   # l=sorted(l,key=lambda x:get_chars(x)[0].fontname)
-    for i in l:
-        f.append(round(get_chars(i)[0].size,2))
-
-    c = Counter(f)   
-    print c
-    for i in l:
-                size = round(pd.Series([j.size for j in get_chars(i)]).mean(),2)
-                
-                text = i.get_text()
-                if  max(f) >size > c.most_common(1)[0][0]:
-                  
-                    q +='<strong>'+text.replace('\n','')+'</strong>'
-
-                elif size == max(f) and n==0:
-                    q += '<header>'+text+'</header>'
-                else :
-                    
-                    q += text.replace('\n','') if '\n' in text and text.strip() else text
-    return q
-def tt(objs,objs_r,objs_l,n):
-    q = ''
-    if objs:
-        regobjs=find_tables(objs)
-        q = gen_xml(regobjs,n)
-    if objs_l:
-        regobjs=find_tables(objs_l)
-        q = gen_xml(regobjs,n)
-    if objs_r:
-        regobjs=find_tables(objs_r)
-        q += gen_xml(regobjs,n)
-    return q
 f = 'C:\\Users\\hellpanerrr\\Downloads\\samples-and-notes\\p1.pdf'
-#f = 'C:\\Users\\hellpanerrr\\Downloads\\samples-and-notes\\GIM1114_Feature Biljecki.pdf'#GIM1214_Feature Rajabifard.pdf'#GIM1114_Feature Biljecki.pdf'
+#f = 'C:\\Users\\hellpanerrr\\Downloads\\samples-and-notes\\GIM1214_Feature Rajabifard.pdf'#GIM1114_Feature Biljecki.pdf'#'#GIM1114_Feature Biljecki.pdf'
 fp = open(f, 'rb')
 # Create a PDF parser object associated with the file object.
 parser = PDFParser(fp)
@@ -201,12 +181,17 @@ laparams = LAParams()
 device = PDFPageAggregator(rsrcmgr, laparams=laparams)
 interpreter = PDFPageInterpreter(rsrcmgr, device)
 
-
-
-q = '<xml>\n'
+root = ET.Element('xml')
+header = ET.SubElement(root,'header')
+title = ET.SubElement(root,'title')
+subtitle = ET.SubElement(root,'subtitle')
+body = ET.SubElement(root,'body')
+body.text=''
+header.text =''
+title.text=' '
+subtitle.text=''
+tree = ET.ElementTree(root)
 for n,page in enumerate(PDFPage.create_pages(document)):
-    
-    
     #if n <> 0: continue
     objs = []
     objs_r = objs_l =''
@@ -214,43 +199,13 @@ for n,page in enumerate(PDFPage.create_pages(document)):
     # receive the LTPage object for the page.
     layout = device.get_result()
     print n,layout.width,layout.height,layout.width/(layout.height*1.0)
-        
     # collecting objects from the all pages, sorting them by their Y coordinate
     objs.append( get_objects(layout))#sorted( get_objects(layout),key=lambda x:x.y0,reverse=True)     )
     objs = objs[0]#sum(objs,[])
-    objs = [i for i in objs if  layout.height*0.05   <= i.y0 <= layout.height-layout.height*0.05 ]
+    #objs = [i for i in objs if  layout.height*0.05   <= i.y0 <= layout.height-layout.height*0.05 ]
     #determines if page is actually has two pages
     if layout.width/(layout.height*1.0) > 0.8:
-        print 'aaa'
         objs_l = [i for i in sorted(objs,key = lambda x:x.x0) if i.x0<= layout.width/2]
         objs_r = [i for i in sorted(objs,key = lambda x:x.x0) if i.x0>  layout.width/2]
-        
-        
-        
-    if  'objs_r' not in globals() or not objs_r:
-        fonts = get_fonts(objs)
-        s = sort_by_x(layout) 
-        l = []
-        for i in sorted(s):
-            l.append(s[i])
-        l = sum(l, [])
-        
-        q+= tt(objs,objs_r,objs_l,n)
-    else:
-          
-        fonts = get_fonts(objs_r)
-        print fonts
-        s = sort_by_x(layout) 
-        l = []
-        for i in sorted(s):
-            l.append(s[i])
-        l = sum(l, [])
-        q+= tt(objs,objs_r,objs_l,n)
-        
-        #q = make_xml(l,q)
-        
-        
-
-q += '</xml>'    
-with open('test.xml','wb') as f:
-    f.write(q.encode('utf-8'))
+        tree = make_xml(objs,objs_r,objs_l,n,tree)
+ET.ElementTree(ET.fromstring(HTMLParser().unescape(ET.tostring(tree, encoding='unicode', method='xml')))).write('test.xml',encoding="UTF-8",xml_declaration=True,pretty_print=True)
